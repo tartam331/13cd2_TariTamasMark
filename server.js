144e485501498ff4b9b5cba7995bdc0e73b07484
tartam331 <tartam331@hengersor.hu>
2025-02-17 14:14:30 +0100

    szerver beállítása
    Mysql szerver kapcsolatának létrehozása.
    Ellenőrző get megírása

--- server.js
+++ server.js
@@ -7,8 +7,30 @@ const bodyparser = require("body-parser");
 app.use(cors());
 app.use(bodyparser.json());
 
+const db = mysql.createConnection(
+
+    {
+        user: "root",
+        host: "localhost",
+        port:3307,
+        password: "",
+        database: "teliolimpia"
+    }
+)
+
+//a szerver futásának ellenőrzése
+
+app.get("/", (req, res) => {
+
+    res.send("A szerver működik!")
+
+
+})
+
+
+
 app.listen(3000,() => {
 
-    console.log('A szerver a 3000 porton fut!')
+    console.log("A szerver a 3000 porton fut!")
 
 })
